58931d36087bd548571f8a59ea33eacd5dbffdcd
王林茜 <157965727+metho1@users.noreply.github.com>
2025-02-04 20:12:58 +0800

    datapanel

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DataPanel/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import DataPanel from './components/DataPanel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DataPanel
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; */
+  height: 100vh;
+  width: 100%;
+  font-family: "微软雅黑";
+  color: #fff;
+  font-size: 16px;
+  background: radial-gradient(90% 90%, #034f8e, #034987, #02366d, #002353);
 }
+
+body{
+  margin: 0px;
+}
+
+
 </style>
